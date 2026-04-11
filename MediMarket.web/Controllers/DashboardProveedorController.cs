1f72df648d6cccb962e61c0e8f2b32574177fdb6
josuenataren23 <josue.nataren23@unach.mx>
2026-04-11 01:40:31 -0600

    Archivos del layout del dashboard de Proveedores

---
+++ MediMarket.web/Controllers/DashboardProveedorController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MediMarket.web.Controllers
+{
+    public class DashboardProveedorController : Controller
+    {
+        // GET: DashboardProveedor
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
